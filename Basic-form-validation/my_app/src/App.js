94cd208a8c089b52b7192b6cb9d842880ffff9d6
Chanchal160817 <you@example.com>
2023-02-02 16:25:12 +0530

    Basic-form-validation

---
+++ Basic-form-validation/my_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './login'
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Form Validation</h1> */}
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
